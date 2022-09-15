fabcc77fec298e76a17fbae0dedc152d5f26d0ed
MohsinTaj <mohsintaj@github.com>
2022-09-15 12:53:40 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Todo from './Components/Todo';
 import html2canvas from 'html2canvas';
 import './index.css'
 const CaptureImage=()=>{
+  html2canvas(document.getElementById("photo")).then(function(canvas){
+    var a=document.createElement('a')
+    a.href=canvas.toDataURL("Pictures").replace("","")
+    a.download="screenshot";
+    a.click();
+  })
+}
+const CaptureImage1=()=>{
   html2canvas(document.body).then(function(canvas){
     var a=document.createElement('a')
     a.href=canvas.toDataURL("Pictures").replace("","")
@@ -15,9 +23,11 @@ function App() {
         <>
         <body>
                 <div>
-                <button className="btn-btn-default" id="button" onClick={CaptureImage} >screenshot</button>
+                <button className="btn-btn-default" id="button" onClick={CaptureImage1} >screenshot</button>
+                <button className="btn-btn-default" id="button" onClick={CaptureImage} >screenshot_only_todo</button>
+                <div id='photo'>
             <Todo/>
-           
+            </div>
         </div>
         </body>
         </>
